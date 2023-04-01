bcaa824bf4c28b34ffc31938422c8cc768abfd56
Alvin Haruna <harunaalvin69@gmail.com>
2023-04-01 09:29:42 +0100

    added function to check link

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 # import urllib
 import urllib.request as urllib
 
+# write function that takes url & returns a response
+def main(url):
+    # use urllib.request to get the data from the url
+    print("checking connectivity...")
+
+    res = urllib.urlopen(url)
+    
+    
+    if res.getcode() == 200:
+        print(f'''connected to {url} successfuly.
+        the response code is {res.getcode()}''')
+    else:
+        print(f'''something went wrong
+        the response code is {res.getcode()}''')
+
 print("This is a site conectivity checker program")
 
 input_url = input("Input the link to the site: ")
 
-# use urllib.request to get the data from the url
-
-# write function that takes url & returns a response
-
-
+main(input_url)
