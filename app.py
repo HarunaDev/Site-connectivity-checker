99a6a352a0dc2feef84909c136e08395c5dc1519
Alvin Haruna <harunaalvin69@gmail.com>
2023-04-01 08:59:35 +0100

    get site link from the user

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # import urllib
 import urllib.request as urllib
 
+print("This is a site conectivity checker program")
+
+input_url = input("Input the link to the site: ")
+
 # use urllib.request to get the data from the url
 
 # write function that takes url & returns a response
