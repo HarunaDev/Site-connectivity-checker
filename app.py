e4be82cd9513057b839788273e57c4d1e8a20954
Alvin Haruna <harunaalvin69@gmail.com>
2023-04-11 13:00:02 +0100

    added code to redirect stderr to error log file

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
-# import urllib
+# import libraries
+import sys
 import urllib.request
+
+# Open file in write mode to redirect stderr
+
+with open("error.log", 'w') as file:
+    # save current stderr to variable
+    stderr_saved = sys.stderr
+
+    # redirect stderr to file
+    sys.stderr = file
 
 # write function that takes url & returns a response
 def main(url):
     # use urllib.request to get the data from the url
     print("checking connectivity...")
 
-    try:
-        res = urllib.request.urlopen(url)
+    with open("error.log", 'w') as file:
+        # save current stderr to variable
+        stderr_saved = sys.stderr
 
-        if res.getcode() == 200:
-            print(f'''connected to {url} successfuly.
-            the response code is {res.getcode()}''')
-        else:
-            print(f'''something went wrong
-            the response code is {res.getcode()}''')
-    except:
-        print("Couldn't establish connection.")
+        # redirect stderr to file
+        sys.stderr = file
+        try:
+            res = urllib.request.urlopen(url)
+
+            if res.getcode() == 200:
+                print(f'''connected to {url} successfuly.
+                the response code is {res.getcode()}''')
+            else:
+                print(f'''something went wrong
+                the response code is {res.getcode()}''')
+        except Exception as e:
+            print("Couldn't establish connection.")
+            print(f"[{e}]", file=sys.stderr)
+
+        sys.stderr = stderr_saved
 
 print("This is a site conectivity checker program")
 
