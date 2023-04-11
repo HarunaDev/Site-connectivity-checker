93701accecf649bce2b0a25cd03ee253409af85a
Alvin Haruna <harunaalvin69@gmail.com>
2023-04-11 11:30:05 +0100

    added try, except block to handle error

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 # import urllib
-import urllib.request as urllib
+import urllib.request
 
 # write function that takes url & returns a response
 def main(url):
     # use urllib.request to get the data from the url
     print("checking connectivity...")
 
-    res = urllib.urlopen(url)
-    
-    
-    if res.getcode() == 200:
-        print(f'''connected to {url} successfuly.
-        the response code is {res.getcode()}''')
-    else:
-        print(f'''something went wrong
-        the response code is {res.getcode()}''')
+    try:
+        res = urllib.request.urlopen(url)
+
+        if res.getcode() == 200:
+            print(f'''connected to {url} successfuly.
+            the response code is {res.getcode()}''')
+        else:
+            print(f'''something went wrong
+            the response code is {res.getcode()}''')
+    except:
+        print("Couldn't establish connection.")
 
 print("This is a site conectivity checker program")
 
